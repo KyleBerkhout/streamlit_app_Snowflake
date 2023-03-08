7f22f3013192909c7ff2039291af981f12fdd7ba
KyleBerkhout <57076076+KyleBerkhout@users.noreply.github.com>
2023-03-08 15:28:46 +1100

    Changed to a try-catch statement.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favourites')
@@ -19,13 +20,26 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+# Fruityvice API Response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered',  fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalised)
+    
+except URLError as e:
+  streamlit.error()
+  
+#streamlit.write('The user entered',  fruit_choice)
 
-fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalised)
+
+
+
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -44,6 +58,7 @@ streamlit.write('Thanks for adding ',  add_my_fruit)
 
 
 
-
+my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')");
+select * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST;
 
 
