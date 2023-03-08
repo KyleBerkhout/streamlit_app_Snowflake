41e35cd62b52fa833db5969f34d7e2d8b26ed86a
KyleBerkhout <57076076+KyleBerkhout@users.noreply.github.com>
2023-03-08 15:15:55 +1100

    Added second box to add more fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,10 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+#Add fruit to list
+add_my_fruit = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('Thanks for adding ',  add_my_fruit)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
 
